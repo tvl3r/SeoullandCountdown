ab6a307966eae78d06a90b716af2488ed12c7687
zpfjqps <zpfjqps@users.noreply.github.com>
2017-12-05 07:07:48 +0900

    Finalllllll
    This reverts commit b806c4892b128c1fbd13619d1d6d0395e8937257.

--- java.js
+++ java.js
@@ -38,3 +38,13 @@ var x = setInterval(function () {
     }
 
 }, 1000);
+
+
+function aud_play_pause() {
+ var myAudio = document.getElementById("myAudio");
+ if (myAudio.paused) {
+    myAudio.play();
+ } else {
+    myAudio.pause();
+ }
+} ;
